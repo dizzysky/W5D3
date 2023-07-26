385a86e9e2e3082b41f96657fb5e8eb323efe7b7
Sanjid <dsanjid@gmail.com>
2023-07-26 11:57:05 -0400

    made tables, working on seeding

--- practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
+++ practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
@@ -1,3 +1,13 @@
+PRAGMA foreign_keys = ON;
+
+
+
+
+
+
+
+DROP TABLE IF EXISTS questions_follows;
+DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -6,7 +16,7 @@ CREATE TABLE users (
     lname TEXT NOT NULL
 );
 
-DROP TABLE IF EXISTS questions;
+
 
 CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
@@ -17,33 +27,8 @@ CREATE TABLE questions (
     FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
-DROP TABLE IF EXISTS question_follows;
 
 CREATE TABLE questions_follows (
-    user_id INTEGER PRIMARY KEY,
-    question_id INTEGER PRIMARY KEY,
-
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (question_id) REFERENCES questions(id)
-);
-
-DROP TABLE IF EXISTS replies;
-
-CREATE TABLE replies (
-    id INTEGER PRIMARY KEY,
-    question_id INTEGER NOT NULL,
-    parent_reply_id INTEGER,
-    author_id INTEGER NOT NULL,
-    body TEXT NOT NULL,
-
-    FOREIGN KEY (question_id) REFERENCES questions(id),
-    FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
-    FOREIGN KEY (author_id) REFERENCES users(id)
-);
-
-DROP TABLE IF EXISTS question_likes;
-
-CREATE TABLE question_likes (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     question_id INTEGER NOT NULL,
@@ -51,3 +36,56 @@ CREATE TABLE question_likes (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+-- DROP TABLE IF EXISTS replies;
+
+-- CREATE TABLE replies (
+--     id INTEGER PRIMARY KEY,
+--     question_id INTEGER NOT NULL,
+--     parent_reply_id INTEGER,
+--     author_id INTEGER NOT NULL,
+--     body TEXT NOT NULL,
+
+--     FOREIGN KEY (question_id) REFERENCES questions(id),
+--     FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
+--     FOREIGN KEY (author_id) REFERENCES users(id)
+-- );
+
+-- DROP TABLE IF EXISTS question_likes;
+
+-- CREATE TABLE question_likes (
+--     id INTEGER PRIMARY KEY,
+--     user_id INTEGER NOT NULL,
+--     question_id INTEGER NOT NULL,
+
+--     FOREIGN KEY (user_id) REFERENCES users(id),
+--     FOREIGN KEY (question_id) REFERENCES questions(id)
+-- );
+
+
+
+
+
+INSERT INTO
+    users (fname, lname)
+VALUES 
+    ('Babe', 'Ruth'),
+    ('Michael', 'Jordan');
+
+INSERT INTO 
+    questions (title, body, author_id)
+VALUES 
+    ('Tuition', 'How much is the full tuition?', (SELECT id from users WHERE fname = 'Michael' AND lname = 'Jordan' ));
+
+
+
+INSERT INTO
+    questions_follows (user_id, question_id)
+VALUES 
+    -- ((SELECT id FROM users WHERE fname = 'Michael'), (SELECT questions.id from questions WHERE questions_follows.user_id = questions.author_id))
+    (1,1)
+
+-- INSERT INTO
+--     replies (question_id, parent_reply_id, author_id, body)
+-- VALUES
+--     ()
